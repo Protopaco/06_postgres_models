9327f2cdfd440bd6ee9bbe5e9c5c11461d92edab
Paul Stevens <twinkisan@hotmail.com>
2020-12-08 14:30:47 -0800

    /insert added & tested

--- index.js
+++ index.js
@@ -1,15 +1,23 @@
 // require('dotenv').config();
-
 const express = require('express');
 const { builtinModules } = require('module');
 const app = express();
+const Dogs = require('./lib/models/dogs.js')
 app.use(express.json());
 
 
 
 app.get('/test', async (req, res) => {
     try {
-        res.send({ greeting: 'HELLO POSTMAN!' })
+        res.status(200).json({ greeting: 'HELLO POSTMAN!' })
+    } catch (e) {
+        res.status(500).text(e.message)
+    }
+})
+
+app.post('/insert', async (req, res) => {
+    try {
+        res.send(await Dogs.insert(req.body))
     } catch (e) {
         res.send(e.message)
     }
--- tests/index.test.js
+++ tests/index.test.js
@@ -3,6 +3,15 @@ const fakeRequest = require('supertest');
 const app = require('../index.js');
 const pool = require('../lib/utils/pool.js');
 const fs = require('fs');
+const testDog = {
+    id: '1',
+    name: 'Marcus',
+    age: 6,
+    breed: 'Terrier',
+    nicknames: ['Ping', 'Boobie', 'Boo']
+}
+
+
 
 describe('test dogs CRUD routes', () => {
     beforeAll(async () => {
@@ -18,4 +27,14 @@ describe('test dogs CRUD routes', () => {
         const expectedResponse = { greeting: 'HELLO POSTMAN!' };
         expect(response.body).toEqual(expectedResponse);
     });
+
+    it('test /insert CRUD route', async () => {
+        const response = await fakeRequest(app)
+            .post('/insert')
+            .send(testDog);
+
+        expect(response.body).toEqual(testDog);
+    })
+
+
 });
